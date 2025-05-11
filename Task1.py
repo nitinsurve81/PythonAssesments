8ed6ef991f6cc29007bea516af21c76e199a30cd
nitinsurve81 <nitinsurve.in@gmail.com>
2025-05-11 20:14:50 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the first number"))
+b = int(input("Enter the second nunber "))
+sum = a + b
+sub = a - b
+multiply = a * b
+div = a/b
+
+print("Addition =", sum) 
+print("Subtraction =", sub)
+print("Multiply =", multiply)
+print("Divison =", div)
+
---
+++ Task2.py
@@ -0,0 +1,5 @@
+fname = input("Enter your first name : ")
+lname = input("Enter your last name : ")
+fullname = fname +" "+lname+"!"
+
+print("Hello,", fullname ,"Welcome to Python Program" )
