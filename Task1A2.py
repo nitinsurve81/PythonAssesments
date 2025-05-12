a66b1487f31d49144d6109cee254e7eec717ee80
nitinsurve81 <nitinsurve.in@gmail.com>
2025-05-12 16:02:36 +0530

    Add files via upload

---
+++ Task1A2.py
@@ -0,0 +1,6 @@
+x = int(input("Enter a number"))
+if x%2 == 0:
+    print(x,"Is a even no")
+else:
+    print(x,"Is a odd no")
+print("Thank you")
---
+++ Task2A2.py
@@ -0,0 +1,4 @@
+j=0
+for i in range(51):
+    j=i+j
+print("The sum of numbers from 1 to 50 is : ",j)
