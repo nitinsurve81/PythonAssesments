5353d8c771e3c242f845a67e04831678e541a887
nitinsurve81 <nitinsurve.in@gmail.com>
2025-05-13 20:04:15 +0530

    Add files via upload

---
+++ Task2A3.py
@@ -0,0 +1,7 @@
+import math
+x = int(input("Enter a number"))
+
+
+print("The square root of ",x ," is : ",math.sqrt(x) )
+print("The log of ",x ," is : ",math.log(x) )
+print("The sin of ",x ," is : ",math.sin(x) )
